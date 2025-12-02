2cc8b4de7416d4e419284003c2b917b15efaa645
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-02 15:26:29 +0530

    Add solution for Design Parking System

--- LeetCode/Design-Parking-System/Solution.cpp
+++ LeetCode/Design-Parking-System/Solution.cpp
@@ -1,23 +1,23 @@
 1class ParkingSystem {
 2public:
-3    int big, medium, small;
+3    int b, m, s;
 4    ParkingSystem(int big, int medium, int small) {
-5        this->big = big;
-6        this->medium = medium;
-7        this->small = small;
+5        b = big;
+6        m = medium;
+7        s = small;
 8    }
 9    
 10    bool addCar(int carType) {
-11        if(carType == 1 && this->big > 0){
-12            this->big -= 1;
+11        if(carType == 1 && b > 0){
+12            b -= 1;
 13            return true;
 14        }
-15        if(carType == 2 && this->medium > 0){
-16            this->medium -= 1;
+15        if(carType == 2 && m > 0){
+16            m -= 1;
 17            return true;
 18        }
-19        if(carType == 3 && this->small > 0){
-20            this->small -= 1;
+19        if(carType == 3 && s > 0){
+20            s -= 1;
 21            return true;
 22        }
 23        return false;
