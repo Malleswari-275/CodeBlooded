c2383810ec83f7b30e73b590fb78bf6dfdace4b3
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-02 07:01:30 +0530

    Add solution for Reshape the Matrix

--- LeetCode/Reshape-the-Matrix/Solution.cpp
+++ LeetCode/Reshape-the-Matrix/Solution.cpp
@@ -4,20 +4,18 @@
 4        int m = mat.size();
 5        int n = mat[0].size();
 6        if(r*c != m*n) return mat;
-7        vector<int>vc;
-8        for(int i = 0; i < m; i++){
-9            for(int j = 0; j < n; j++) {
-10                vc.push_back(mat[i][j]);
-11            }
-12        }
-13        vector<vector<int>>ans(r,vector<int>(c));
-14        int k = 0;
-15        for(int i = 0; i < r; i++){
-16            for(int j = 0; j < c; j++){
-17                ans[i][j] = vc[k];
-18                k++;
-19            }
-20        }
-21        return ans;
-22    }
-23};
+7        vector<vector<int>>ans(r,vector<int>(c));
+8        int k = 0, l = 0;
+9        for(int i = 0; i < r; i++){
+10            for(int j = 0; j < c; j++){
+11                ans[i][j] = mat[k][l];
+12                l++;
+13                if(l >= n){
+14                    l = 0;
+15                    k++;
+16                }
+17            }
+18        }
+19        return ans;
+20    }
+21};
