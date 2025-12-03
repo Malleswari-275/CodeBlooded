d06be271efda1b31b56cd4bddccb452f80d70a2f
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-03 06:35:58 +0530

    Add solution for Spiral Matrix II

--- LeetCode/Spiral-Matrix-II/Solution.cpp
+++ LeetCode/Spiral-Matrix-II/Solution.cpp
@@ -2,20 +2,18 @@
 2public:
 3    vector<vector<int>> generateMatrix(int n) {
 4        vector<vector<int>>vc(n,vector<int>(n));
-5        vector<int>arr;
-6        for(int i = 0; i < n*n; i++)arr.push_back(i+1);
-7        int k = 0;
-8        int l = 0, r = n-1, t = 0, b = n-1;
-9        while(t <= b && l <= r){
-10            for(int i = l; i <= r; i++) vc[t][i] = arr[k++];
-11            t++;
-12            for(int i = t; i <= b; i++) vc[i][r] = arr[k++];
-13            r--;
-14            for(int i = r; i >= l; i--) vc[b][i] = arr[k++];
-15            b--;
-16            if(l <= r) for(int i = b; i >= t; i--) vc[i][l] = arr[k++];
-17            l++;
-18        }
-19        return vc;
-20    }
-21};
+5        int k = 1;
+6        int l = 0, r = n-1, t = 0, b = n-1;
+7        while(t <= b && l <= r){
+8            for(int i = l; i <= r; i++) vc[t][i] = k++;
+9            t++;
+10            for(int i = t; i <= b; i++) vc[i][r] = k++;
+11            r--;
+12            for(int i = r; i >= l; i--) vc[b][i] = k++;
+13            b--;
+14            if(l <= r) for(int i = b; i >= t; i--) vc[i][l] = k++;
+15            l++;
+16        }
+17        return vc;
+18    }
+19};
