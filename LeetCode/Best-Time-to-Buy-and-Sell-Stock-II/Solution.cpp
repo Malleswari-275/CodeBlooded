d2069b4b4858bfed6d86273092d5444b206c1561
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-17 06:05:55 +0530

    Add solution for Best Time to Buy and Sell Stock II

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.cpp
@@ -1,10 +1,10 @@
 1class Solution {
 2public:
 3    int maxProfit(vector<int>& prices) {
-4        int profit = 0;
+4        int total_profit = 0;
 5        for(int i = 1; i < prices.size(); i++){
-6            if(prices[i] > prices[i-1]) profit += prices[i]-prices[i-1];
+6            if(prices[i] > prices[i-1]) total_profit += prices[i]-prices[i-1];
 7        }
-8        return profit;
+8        return total_profit;
 9    }
 10};
