7fa697fdc1d634b19ad5c080d98f89dc541ef672
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-20 14:53:16 +0530

    Add solution for Find All People With Secret

--- LeetCode/Find-All-People-With-Secret/Solution.cpp
+++ LeetCode/Find-All-People-With-Secret/Solution.cpp
@@ -16,35 +16,34 @@
 16
 17        while(!q.empty()){
 18            auto [p,t] = q.front();
-19            cout<<p<<" "<<t<<endl;
-20            q.pop();
-21            for(auto x : adj[p]){
-22                if(t <= x.second && visited[x.first] > x.second)
-23                {
-24                    secrets.insert(x.first);
-25                    q.push(x);
-26                    visited[x.first] = x.second;
-27                }
-28            }
-29        }
-30
-31    }
-32    static bool cmp(vector<int>&a, vector<int>&b){
-33        return a[2] < b[2];
-34    }
-35    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-36        vector<vector<pair<int,int>>>adj(n);
-37       sort(meetings.begin(),meetings.end(),cmp);
-38        for(int i = 0; i < meetings.size(); i++){
-39            int u = meetings[i][0];
-40            int v = meetings[i][1];
-41            int t = meetings[i][2];
-42            adj[u].push_back({v,t});
-43            adj[v].push_back({u,t});
-44        }
-45
-46        BFS(adj,n,firstPerson);
-47        vector<int> v(secrets.begin(),secrets.end());
-48        return v;
-49    }
-50};
+19            q.pop();
+20            for(auto x : adj[p]){
+21                if(t <= x.second && visited[x.first] > x.second)
+22                {
+23                    secrets.insert(x.first);
+24                    q.push(x);
+25                    visited[x.first] = x.second;
+26                }
+27            }
+28        }
+29
+30    }
+31    static bool cmp(vector<int>&a, vector<int>&b){
+32        return a[2] < b[2];
+33    }
+34    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+35        vector<vector<pair<int,int>>>adj(n);
+36       sort(meetings.begin(),meetings.end(),cmp);
+37        for(int i = 0; i < meetings.size(); i++){
+38            int u = meetings[i][0];
+39            int v = meetings[i][1];
+40            int t = meetings[i][2];
+41            adj[u].push_back({v,t});
+42            adj[v].push_back({u,t});
+43        }
+44
+45        BFS(adj,n,firstPerson);
+46        vector<int> v(secrets.begin(),secrets.end());
+47        return v;
+48    }
+49};
