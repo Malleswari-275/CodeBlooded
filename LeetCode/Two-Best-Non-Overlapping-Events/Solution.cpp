343f2dd4d5330aded74e360a7f1392e89b97e6cc
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-23 18:50:20 +0530

    Add solution for Two Best Non-Overlapping Events

--- LeetCode/Two-Best-Non-Overlapping-Events/Solution.cpp
+++ LeetCode/Two-Best-Non-Overlapping-Events/Solution.cpp
@@ -1,28 +1,25 @@
 1class Solution {
 2public:
 3    static bool cmp(vector<int>&a, vector<int>&b){
-4        // if(a[1] != b[1]) 
-5        return a[1] < b[1];
-6        // return a[0] < b[0];
-7    }
-8    int maxTwoEvents(vector<vector<int>>& events) {
-9        sort(events.begin(),events.end(),cmp);
-10        vector<int>maxi(events.size());
-11        maxi[0] = events[0][2];
-12        for(int i = 1; i < events.size(); i++){
-13            maxi[i] = max(events[i][2],maxi[i-1]);
-14        }
-15        int ans = 0;
-16        for(int i = 0; i < events.size(); i++){
-17            int start = events[i][0];
-18            auto it = lower_bound(events.begin(),events.end(),start,[](const vector<int>&a, int val){
-19                return a[1] < val;
-20            });
-21            int idx = it - events.begin();
-22            cout<<idx;
-23            ans = max(ans, events[i][2]);
-24            if(idx != 0)ans = max(ans,maxi[idx-1]+events[i][2]);
-25        }
-26        return ans;
-27    }
-28};
+4        return a[1] < b[1];
+5    }
+6    int maxTwoEvents(vector<vector<int>>& events) {
+7        sort(events.begin(),events.end(),cmp);
+8        vector<int>maxi(events.size());
+9        maxi[0] = events[0][2];
+10        for(int i = 1; i < events.size(); i++){
+11            maxi[i] = max(events[i][2],maxi[i-1]);
+12        }
+13        int ans = 0;
+14        for(int i = 0; i < events.size(); i++){
+15            int start = events[i][0];
+16            auto it = lower_bound(events.begin(),events.end(),start,[](const vector<int>&a, int val){
+17                return a[1] < val;
+18            });
+19            int idx = it - events.begin();
+20            ans = max(ans, events[i][2]);
+21            if(idx != 0)ans = max(ans,maxi[idx-1]+events[i][2]);
+22        }
+23        return ans;
+24    }
+25};
