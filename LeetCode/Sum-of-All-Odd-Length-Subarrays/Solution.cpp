95e93fdd20d813cfecfbeead500fd9e66746aa47
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-23 14:24:18 +0530

    Add solution for Sum of All Odd Length Subarrays

--- LeetCode/Sum-of-All-Odd-Length-Subarrays/Solution.cpp
+++ LeetCode/Sum-of-All-Odd-Length-Subarrays/Solution.cpp
@@ -3,14 +3,11 @@
 3    int sumOddLengthSubarrays(vector<int>& arr) {
 4        int sum = 0;
 5        for(int i = 0; i < arr.size(); i++){
-6            for(int j = i; j < arr.size(); j++){
-7                if((j-i+1)%2 != 0){
-8                    for(int k = i; k <= j; k++){
-9                        sum += arr[k];
-10                    }
-11                }
-12            }
-13        }
-14        return sum;
-15    }
-16};
+6            int contrib = (i+1)*(arr.size()-i);
+7            double odd = contrib/2.0;
+8            if(arr.size()%2 != 0) odd = ceil(odd);
+9            sum += odd*arr[i];
+10        }
+11        return sum;
+12    }
+13};
