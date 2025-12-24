2a2e7fadd879565c33587533f280917843b24345
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2025-12-24 15:43:47 +0530

    Add solution for Optimal Partition of String

---
+++ LeetCode/Optimal-Partition-of-String/Solution.cpp
@@ -0,0 +1,18 @@
+1class Solution {
+2public:
+3    int partitionString(string s) {
+4        vector<int>freq(26,0);
+5        int cnt = 0;
+6        // start = 0;
+7        for(int i = 0; i < s.size(); i++){
+8            freq[s[i]-'a']++;
+9            if(freq[s[i]-'a'] > 1){
+10                cnt++;
+11                fill(freq.begin(),freq.end(),0);
+12                freq[s[i]-'a']++;
+13            }
+14            
+15        }
+16        return cnt+1;
+17    }
+18};
