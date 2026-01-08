c6fb3051663a588be7a9af6cacb2e4c44736b4d6
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2026-01-08 14:04:15 +0530

    Add solution for Single Number

---
+++ LeetCode/Single-Number/Solution.cpp
@@ -0,0 +1,10 @@
+1class Solution {
+2public:
+3    int singleNumber(vector<int>& nums) {
+4        int ans = 0;
+5        for(int i = 0; i < nums.size(); i++){
+6            ans = ans^nums[i];
+7        }
+8        return ans;
+9    }
+10};
