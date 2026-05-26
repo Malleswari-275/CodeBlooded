affe16c9951d386ef3fa5983810585d329c09bf3
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2026-05-26 10:39:51 +0530

    Add solution for Maximum Depth of Binary Tree

--- LeetCode/Maximum-Depth-of-Binary-Tree/Solution.cpp
+++ LeetCode/Maximum-Depth-of-Binary-Tree/Solution.cpp
@@ -12,17 +12,20 @@
 12class Solution {
 13public:
 14    int maxi = INT_MIN;
-15    void inorder(TreeNode* root, int curr){
+15    void inorder(TreeNode* root, int &curr){
 16        if(!root){
 17            maxi = max(maxi,curr);
 18            return;
 19        }
 20        curr += 1;
-21        inorder(root->left,curr);
-22        inorder(root->right,curr);
-23    }
-24    int maxDepth(TreeNode* root) {
-25        inorder(root,0);
-26        return maxi;
-27    }
-28};
+21        cout<<curr<<endl;
+22        inorder(root->left,curr);
+23        inorder(root->right,curr);
+24        curr -= 1;
+25    }
+26    int maxDepth(TreeNode* root) {
+27        int c = 0;
+28        inorder(root,c);
+29        return maxi;
+30    }
+31};
