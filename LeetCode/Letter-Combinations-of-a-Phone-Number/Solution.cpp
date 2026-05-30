08a049c5f4153ce6806ede91b3916f849b8dffaa
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2026-05-30 11:55:16 +0530

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.cpp
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.cpp
@@ -1,34 +1,33 @@
 1class Solution {
 2public:
-3    
-4
-5    vector<string>vc;
-6    void backtrack(string digits, map<char,vector<char>>&mp, string st, int i){
-7        if(i == digits.size()){
-8            vc.push_back(st);
-9            return;
-10        }
-11        if(i >= digits.size()) return;
-12        vector<char>ch = mp[digits[i]];
-13        for(int j = 0; j < ch.size(); j++){
-14            st += ch[j];
-15            backtrack(digits,mp,st,i+1);
-16            st.pop_back();
-17        }
-18    }
-19    vector<string> letterCombinations(string digits) {
-20        map<char,vector<char>>mp;
-21        mp['2'] = {'a','b','c'};
-22        mp['3'] = {'d', 'e', 'f'};
-23        mp['4'] = {'g', 'h', 'i'};
-24        mp['5'] = {'j', 'k', 'l'};
-25        mp['6'] = {'m', 'n', 'o'};
-26        mp['7'] = {'p', 'q', 'r', 's'};
-27        mp['8'] = {'t', 'u', 'v'};
-28        mp['9'] = {'w', 'x', 'y', 'z'};
-29
-30        backtrack(digits,mp,"",0);
-31        return vc;
-32        
-33    }
-34};
+3    vector<string> combo(map<int, vector<string>>& mp, string s, int n) {
+4        if (n == 0) return mp[s[0] - '0'];
+5        vector<string> v = combo(mp, s, n - 1);
+6        vector<string> ans;
+7        vector<string> vc = mp[s[n] - '0'];
+8        for (int i = 0; i < v.size(); i++) {
+9            for (int j = 0; j < vc.size(); j++) {
+10                ans.push_back(v[i] + vc[j]);
+11            }
+12        }
+13        return ans;
+14    }
+15
+16    vector<string> letterCombinations(string digits) {
+17        vector<string> vc;
+18        if (digits.empty()) return vc; 
+19        map<int, vector<string>> mp;
+20        mp[2] = {"a", "b", "c"};
+21        mp[3] = {"d", "e", "f"};
+22        mp[4] = {"g", "h", "i"};
+23        mp[5] = {"j", "k", "l"};
+24        mp[6] = {"m", "n", "o"};
+25        mp[7] = {"p", "q", "r", "s"};
+26        mp[8] = {"t", "u", "v"};
+27        mp[9] = {"w", "x", "y", "z"};
+28
+29        vc = combo(mp, digits, digits.size() - 1);
+30        return vc;
+31    }
+32};
+33
