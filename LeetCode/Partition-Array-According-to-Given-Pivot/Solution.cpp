8fcc9a51b705f3761bcf504f4d2cd7ef9b175059
Naga Malleswari Alajingi <23A91A6166@aec.edu.in>
2026-06-08 09:47:30 +0530

    Add solution for Partition Array According to Given Pivot

--- LeetCode/Partition-Array-According-to-Given-Pivot/Solution.cpp
+++ LeetCode/Partition-Array-According-to-Given-Pivot/Solution.cpp
@@ -3,20 +3,20 @@
 3    vector<int> pivotArray(vector<int>& nums, int pivot) {
 4        vector<int>vc(nums.size());
 5        int i = 0;
-6        for(int j = 0; j < nums.size(); j++){
-7            if(nums[j] < pivot){
-8                vc[i] = nums[j];
-9                i++;
-10            }
-11        }
-12        for(int j = 0; j < nums.size(); j++){
-13            if(nums[j] == pivot){
-14                vc[i] = nums[j];
-15                i++;
+6        int k = nums.size()-1;
+7        int n = nums.size();
+8        for(int j = 0; j < nums.size(); j++){
+9            if(nums[j] < pivot){
+10                vc[i] = nums[j];
+11                i++;
+12            }
+13            if(nums[n-j-1] > pivot){
+14                vc[k] = nums[n-j-1];
+15                k--;
 16            }
 17        }
 18        for(int j = 0; j < nums.size(); j++){
-19            if(nums[j] > pivot){
+19            if(nums[j] == pivot){
 20                vc[i] = nums[j];
 21                i++;
 22            }
